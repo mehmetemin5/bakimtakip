d7618ac0034c7f89c21ccf45eb0d3cc3ded60b76
mehmetemin5 <90.mehmetemin@gmail.com>
2026-02-09 19:53:38 +0300

    Update GAS_URL to new Google Apps Script link

--- config.js
+++ config.js
@@ -2,4 +2,5 @@
  * Google Apps Script Web App adresi.
  * GAS projesinde: Dağıtım → Web uygulaması → Dağıt → Çıkan URL'yi buraya yapıştırın (exec ile biten).
  */
-var GAS_URL = 'https://script.google.com/macros/s/BURAYA_KENDI_LINKINIZI_YAPIŞTIRIN/exec';
+var GAS_URL = 'https://script.google.com/macros/s/AKfycbxsKmAB7uHJZlcN7WGmyoQfj8jp7fyXn8Oe37y1KuRK36cf_1NF8SWRoOxoHmZUK14/exec';
+
